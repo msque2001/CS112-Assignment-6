d4d277d77d4768e1ba301c7b6c3091fbc40929b6
Muhammad Saaim Qureshi <saaimque@gmail.com>
2020-06-08 22:52:46 +0500

    Array polynomial addition done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <cmath>
 using namespace std;
 
-#define FILE "./sample_inputs/example1-10x5.txt"
+#define INPUT_FILE "./sample_inputs/example1-10x5.txt"
+#define OUTPUT_FILE "result_addition.txt"
 
 class ArrayPoly
 {
@@ -15,22 +16,30 @@ class ArrayPoly
         {
 
             ifstream data;
-            data.open(FILE);
+            data.open(INPUT_FILE);
             data.seekg(2, ios::beg);
             data >> numOfCoff;
-           
-           polynomial = new int[numOfCoff];
+        
+            polynomial = new int[numOfCoff];
 
-            data.seekg(currentG, ios::beg);
-            for(int i = 0; i < numOfCoff; i++)
-                data >> polynomial[i];
+            if(currentG != -1)
+            {
+                data.seekg(currentG, ios::beg);
+                for(int i = 0; i < numOfCoff; i++)
+                    data >> polynomial[i];
 
-            currentG = data.tellg(); // UPDATING CURRENT GETTER POINTER LOCATION FOR NEXT POLY
+                currentG = data.tellg(); // UPDATING CURRENT GETTER POINTER LOCATION FOR NEXT POLY
+            }
 
             data.close();
-           
         }
 
+        ~ArrayPoly()
+        {
+            polynomial = NULL;
+            delete[] polynomial;
+        }
+/*
         int calculateSum(int X)
         {
             int sum = 0;
@@ -41,33 +50,76 @@ class ArrayPoly
 
             return sum;
 
+        } */
+
+        int *getPoly()
+        {
+          return polynomial;
         }
+
+        ArrayPoly addPoly(ArrayPoly p2)
+        {
+            ArrayPoly tempPoly;
+            
+            for(int i = 0; i < numOfCoff; i++)
+                tempPoly.polynomial[i] = polynomial[i] + p2.polynomial[i];
+
+            return tempPoly;
+        }
+
+
 
         
 
 };
 
-int calculateSumOfArrayPoly(ArrayPoly polys[], int numOfPoly, int var)
-{
-    int sum = 0;
-    for(int i = 0; i < numOfPoly; i++)
-        sum += polys[i].calculateSum(var);
-    return sum;
-}
+void sumOfArrayPoly();
+
 
 int ArrayPoly::currentG = 5;
 
 int main()
 {
     int X = 2;
-    ArrayPoly polys[10];
-    for(int i = 0; i < 10; i++)
-        cout << polys[i].calculateSum(X) << " + ";
     
-    cout << endl << "= " << calculateSumOfArrayPoly(polys, 10, X);
-    
-
+    sumOfArrayPoly();
 
 
     return 0;
+}
+
+void sumOfArrayPoly()
+{
+    int numOfPolys, numOfCoff;
+
+    fstream data;
+    data.open(INPUT_FILE, ios::in);
+    data >> numOfPolys >> numOfCoff;
+    data.close();
+
+    ArrayPoly polys[numOfPolys + 1];
+    
+    polys[numOfPolys] = polys[0].addPoly(polys[1]);
+
+    for(int i = 2; i < numOfPolys; i++)
+        polys[numOfPolys] = polys[i].addPoly(polys[numOfPolys]);
+
+    data.open(OUTPUT_FILE, ios::out);
+
+    data << "New Polynomial = ";
+    for(int i = 0; i < numOfCoff; i++)
+    {
+        data << polys[numOfPolys].getPoly()[i] << " ";
+    }
+
+    data.close();
+
+    data.open(OUTPUT_FILE, ios::in);
+
+    string newPoly;
+    getline(data, newPoly);
+
+    cout << newPoly << endl;
+
+    data.close();
 }
